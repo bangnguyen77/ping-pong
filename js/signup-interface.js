f6c767bf8f99c868f6adca353e2d1661707a27e7
Bang Nguyen <bangnv77@gmail.com>
2016-07-31 19:19:00 -0700

    end of lesson 'JSHint'

--- js/signup-interface.js
+++ js/signup-interface.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#signup").submit(function(event) {
     event.preventDefault();
     var email = $("#email").val();
-    $("#signup").hide()
+    $("#signup").hide();
     $("#solution").prepend("<p>Thank you, " + email + " has been added to our list!</p>");
   });
 });
