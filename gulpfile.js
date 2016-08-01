f3f9e206023658c94c9c7f0d037cef3647905424
Bang Nguyen <bangnv77@gmail.com>
2016-07-31 18:57:00 -0700

    end of lesson 'Minification'

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 var browserify = require("browserify");
+var uglify = require("gulp-uglify");
 var source = require("vinyl-source-stream");
 
 gulp.task("concatInterface", function() {
@@ -15,3 +16,9 @@ gulp.task("jsBrowserify", ['concatInterface'], function() {
     .pipe(source("app.js"))
     .pipe(gulp.dest("./build/js"));
 });
+
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+})
