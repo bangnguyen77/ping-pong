9621baae082cbd5e1da87f25ea8aac828b659775
Bang Nguyen <bangnv77@gmail.com>
2016-07-31 18:48:11 -0700

    end of lesson 'Concatenation' for weekend homework

---
+++ tmp/allConcat.js
@@ -0,0 +1,24 @@
+// user interface logic
+var Calculator = require('./../js/pingpong.js').calculatorModule;
+
+$(document).ready(function() {
+  $("#ping-pong-form").submit(function(event) {
+    event.preventDefault();
+    // $("ul#solution li").remove(); // remove bullets?
+    var goal = $("#goal").val();
+    var simpleCalculator = new Calculator("hot pink");
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $("#solution").append("<li>" + element + "</li>");
+    });
+  });
+});
+
+$(document).ready(function() {
+  $("#signup").submit(function(event) {
+    event.preventDefault();
+    var email = $("#email").val();
+    $("#signup").hide()
+    $("#solution").prepend("<p>Thank you, " + email + " has been added to our list!</p>");
+  });
+});
